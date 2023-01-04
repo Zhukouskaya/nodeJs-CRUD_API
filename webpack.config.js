195f3f70bfdb90be69910a077b1f89d187f84428
Lizaveta_Zhukouskaya <lizaveta.zhukouskaya@gmail.com>
2023-01-04 14:52:54 +0300

    refactor: add .env, update ts and webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,31 @@
-const path = require('path');
+const path =  require('path');
+
+const includePaths = [
+  path.resolve(__dirname, 'src'),
+  path.resolve(__dirname, 'test'),
+  path.resolve(__dirname, 'package.json'),
+];
 
 module.exports = {
+  
   entry: './src/index.ts',
   output: {
     path: path.resolve(__dirname, 'dist'),
-    filename: 'bundle.js',
+    filename: 'bundle.js'
+  },
+  mode: 'development',
+  module: {
+    rules: [
+      {
+        test: /.(ts)$/i,
+        use: 'ts-loader',
+        exclude: ['/node_modules/'],
+        include: includePaths,
+      },
+    ]
   },
   resolve: {
-    extensions: ['.ts', '.js']
+    extensions: ['.js','.ts'],
+    modules: ['src', 'node_modules']
   }
-};
+}
